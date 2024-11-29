49d1b405c475c1a44ed34b362c6eda7cee3717cd
shenghuo2 <1308357113@qq.com>
2024-11-29 17:30:04 +0800

    feat: chapter6 hashMap (2024.11.29)

---
+++ src/cn/shenghuo2/practice6/TeacherExample14.java
@@ -0,0 +1,15 @@
+package cn.shenghuo2.practice6;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class TeacherExample14 {
+    public static void main(String[] args) {
+        HashMap hm = new HashMap<>();
+        hm.put("zhangsan",90);
+        hm.put("lisi",100);
+        hm.put("wangwu",80);
+        hm.put("zhangsan",95);
+
+    }
+}
