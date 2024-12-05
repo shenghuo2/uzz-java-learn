77ae343c9b66804051e7f15c53ff50c769773b33
shenghuo2 <1308357113@qq.com>
2024-12-05 16:34:45 +0800

    feat: chapter 7 IOStream copy file use BufferedIOStream (2024.12.5)
    time used: 181

--- src/cn/shenghuo2/practice7/TeacherExample13.java
+++ src/cn/shenghuo2/practice7/TeacherExample13.java
@@ -1,6 +1,8 @@
 package cn.shenghuo2.practice7;
 
 // 2024.12.5 周四下午 78节
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
@@ -8,16 +10,19 @@ public class TeacherExample13 {
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("/Users/shenghuo2/Downloads/Timna_3eaa044535d9b8d3289899d9f9c00119.zip");
         FileOutputStream fos = new FileOutputStream("target.zip");
-
-        int b; 
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        int b;
         long a = System.currentTimeMillis();
-        while ((b = fis.read()) != -1) {
-            fos.write(b);
+        while ((b = bis.read()) != -1) {
+            bos.write(b);
         }
         long c = System.currentTimeMillis();
         System.out.println("time used: " + (c - a));
+
         fis.close();
         fos.close();
-
+        bis.close();
+        bos.close();
     }
 }
