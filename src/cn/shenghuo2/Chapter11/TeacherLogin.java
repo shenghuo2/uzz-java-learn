09cb6bacf9ccae18ba8671a7c781859395a11521
shenghuo2 <1308357113@qq.com>
2024-12-05 20:55:04 +0800

    feat: chapter 11 JFrame (2024.12.5)

---
+++ src/cn/shenghuo2/Chapter11/TeacherLogin.java
@@ -0,0 +1,15 @@
+package cn.shenghuo2.Chapter11;
+// 2024.12.5  下午78节
+import javax.swing.*;
+
+public class TeacherLogin {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(400,200);
+//        frame.setLocation(300,200);
+
+        JButton b1 = new JButton("确定");
+        frame.add(b1);
+    }
+}
